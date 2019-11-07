d6ace83baed6005b430386651c60e61f76619fc8
tcrasset <tomcrasset@gmail.com>
2019-11-07 18:33:59 +0100

    Add script to copy files to HPC Cluster

---
+++ copytoserver.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Change this with your personal information
+rsaPath=/home/tom/.ssh/id_rsa.ceci
+user=tcrasset
+server=nic4.segi.ulg.ac.be
+homePath=/home/tom/Documents/Uliege/Master2/HPC/Project2
+serverPath=/home/ulg/info0939/tcrasset/Project2
+
+ssh-add $rsaPath
+
+# Cycles through a list of filenames in filesToCopyToServer.txt
+# and copies them to the server
+while IFS="" read -r p || [ -n "$p" ]
+do
+scp -i $rsaPath $homePath/$p $user@$server:$serverPath
+done < filesToCopyToServer.txt
