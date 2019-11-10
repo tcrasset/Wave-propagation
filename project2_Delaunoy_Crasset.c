b6691d433760583c210ab788fb1227a0b0f57399
tcrasset <tomcrasset@gmail.com>
2019-11-10 12:06:15 +0100

    Use structures to represent parameters and map

--- project2_Delaunoy_Crasset.c
+++ project2_Delaunoy_Crasset.c
@@ -3,78 +3,7 @@
 #include <stdlib.h>
 #include <sys/types.h>
 
-
-void readParameterFile(const char* filename, double* g, double* gamma, 
-                        double* deltaX, double* deltaY, double* deltaT, 
-                        double* A, double* func, unsigned int* S, 
-                        unsigned int* s, double* r_threshold){
-    FILE * fp;
-
-    fp = fopen(filename, "r");
-    if (fp == NULL){
-        exit(EXIT_FAILURE);
-    }
-
-    fscanf(fp,"%lf\n%lf\n%lf\n%lf\n%lf\n%lf\n%lf\n%lf\n%u\n%u\n%lf", g, gamma, deltaX, deltaY, deltaT, A, func, S, s, r_threshold);
-    fclose(fp);
-
-    return;
-}
-
-double* readMapFile(const char* filename, double* a, double* b, 
-                        long long* X,  long long* Y){
-    FILE * fp;
-
-    fp = fopen(filename, "rb");
-    if (fp == NULL){
-        exit(EXIT_FAILURE);
-    }
-
-    // Read the parameters at the top of the map file
-    // Assumption : there are no spaces and no end of lines, just
-    // contiguous bytes in double precision (8 bytes per unit)
-    char buffer[8];
-    fread(buffer, 8, 1, fp);
-    a = ((double*)buffer);
-
-    fread(buffer, 8, 1, fp);
-    b = ((double*)buffer);
-
-    fread(buffer, 8, 1, fp);
-    X = ((long long*)buffer);
-
-    fread(buffer, 8, 1, fp);
-    Y = ((long long*)buffer);
-    
-    double* grid = malloc((*X) * (*Y) * sizeof(double));
-    
-    if (grid == NULL){
-        fclose(fp);
-        exit(EXIT_FAILURE);
-    }
-
-    // While we still read 8 contiguous bytes,
-    // fill the array
-    long long i = 0;
-    while(fread(buffer, 8, 1, fp) == 8) {
-        grid[i] = *((double*)buffer);
-    }
-
-    fclose(fp);
-    return grid;
-}
-
-int main(int argc, char const* argv[]) {
-
-    // Check number of arguments
-    assert(argc == 4);  
-    const char* parameter_file = argv[1];
-    const char* map_file = argv[2];
-    const unsigned int scheme = atoi(argv[3]);
-    
-    //Check argument validity
-    assert((scheme == 0) || (scheme == 1));
-
+typedef struct Parameters {
     double g;
     double gamma;
     double deltaX;
@@ -85,15 +14,119 @@ int main(int argc, char const* argv[]) {
     unsigned int S;
     unsigned int s;
     double r_threshold;
+} Parameters;
 
-    //readParameterFile(parameter_file, &g, &gamma, &deltaX, &deltaY, &deltaT, &A, &func, &S, &s, &r_threshold);
-
-    double a; 
-    double b; 
-    long long X;  
+typedef struct Map {
+    double a;
+    double b;
+    long long X;
     long long Y;
-    double* grid = readMapFile(map_file, &a, &b, &X, &Y);
+    double* grid;
+} Map;
 
+Parameters* readParameterFile(const char* filename) {
+    FILE* fp;
+
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Unable to open file %s\n", filename);
+        exit(EXIT_FAILURE);
+    }
+
+    Parameters* params = malloc(sizeof(Parameters));
+
+    if (params == NULL) {
+        fprintf(stderr, "Unable to allocate memory for parameters\n");
+        free(fp);
+        exit(EXIT_FAILURE);
+    }
+
+    fscanf(fp, "%lf", &params->g);
+    fscanf(fp, "%lf", &params->gamma);
+    fscanf(fp, "%lf", &params->deltaX);
+    fscanf(fp, "%lf", &params->deltaY);
+    fscanf(fp, "%lf", &params->deltaT);
+    fscanf(fp, "%lf", &params->A);
+    fscanf(fp, "%lf", &params->func);
+    fscanf(fp, "%u", &params->S);
+    fscanf(fp, "%u", &params->s);
+    fscanf(fp, "%lf", &params->r_threshold);
+
+    fclose(fp);
+
+    return params;
+}
+
+Map* readMapFile(const char* filename) {
+    FILE* fp;
+
+    fp = fopen(filename, "rb");
+    if (fp == NULL) {
+        fprintf(stderr, "Unable to open file %s\n", filename);
+        exit(EXIT_FAILURE);
+    }
+
+    // Read the parameters at the top of the map file
+    // Assumption : there are no spaces and no end of lines, just
+    // contiguous bytes in double precision (8 bytes per unit)
+
+    Map* map = malloc(sizeof(Map));
+
+    if (map == NULL) {
+        fprintf(stderr, "Unable to allocate memory for the map\n");
+        free(fp);
+        exit(EXIT_FAILURE);
+    }
+
+    char buffer[8];
+    fread(buffer, 8, 1, fp);
+    map->a = *((double*)buffer);
+
+    fread(buffer, 8, 1, fp);
+    map->b = *((double*)buffer);
+
+    fread(buffer, 8, 1, fp);
+    map->X = *((long long*)buffer);
+
+    fread(buffer, 8, 1, fp);
+    map->Y = *((long long*)buffer);
+
+    // map->grid = malloc(map->X * map->Y * sizeof(double));
+
+    // if (map->grid == NULL) {
+    //     fprintf(stderr, "Unable to allocate memory for the grid\n");
+    //     free(map);
+    //     fclose(fp);
+    //     exit(EXIT_FAILURE);
+    // }
+
+    // // While we still read 8 contiguous bytes,
+    // // fill the array
+    // long long i = 0;
+    // while (fread(buffer, 8, 1, fp) == 8) {
+    //     map->grid[i] = *((double*)buffer);
+    // }
+
+    fclose(fp);
+
+    return map;
+}
+
+int main(int argc, char const* argv[]) {
+    // Check number of arguments
+    assert(argc == 4);
+    const char* parameter_file = argv[1];
+    const char* map_file = argv[2];
+    const unsigned int scheme = atoi(argv[3]);
+
+    //Check argument validity
+    assert((scheme == 0) || (scheme == 1));
+
+    Parameters* param = readParameterFile(parameter_file);
+    Map* map = readMapFile(map_file);
+
+    free(param);
+    free(map);
     // Explicit
     if (scheme == 0) {
         printf("Explicit");
