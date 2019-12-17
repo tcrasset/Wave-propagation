3be4c9274fdd3453045cdddf5227cd12427d98bf
Arnaud Delaunoy <arnaud.delaunoy@student.uliege.be>
2019-12-17 14:06:11 +0100

    sparse conjugate gradient works

--- project2_Delaunoy_Crasset_IMPLICIT.h
+++ project2_Delaunoy_Crasset_IMPLICIT.h
@@ -1,8 +1,11 @@
 #ifndef IMPLICIT_H_
 #define IMPLICIT_H_
 
+#include "project2_Delaunoy_Crasset_SPARSE.h"
+
 double dotProduct(double* x, double* y, unsigned int size);
 double vectorNorm(double* x, unsigned int size);
 double* conjugateGradient(double** A, double* b, unsigned int size, double rThresh);
+double* sparseConjugateGradient(SparseMatrix* A, double* b, unsigned int size, double rThresh);
 
 #endif
