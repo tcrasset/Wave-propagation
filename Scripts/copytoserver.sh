4ad71a17b6de7efc1b673f0953811094cd22734b
tcrasset <tomcrasset@gmail.com>
2019-12-19 21:16:09 +0100

    Modify several scripts

--- Scripts/copytoserver.sh
+++ Scripts/copytoserver.sh
@@ -12,7 +12,8 @@ ssh-add $rsaPath
 
 # Code
 scp -i $rsaPath $homePath/project2_Delaunoy_Crasset_MAIN.c $user@$server:$serverPath
-scp -i $rsaPath $homePath/project2_Delaunoy_Crasset_MAIN.h $user@$server:$serverPath
+scp -i $rsaPath $homePath/project2_Delaunoy_Crasset_EXPLICIT.c $user@$server:$serverPath
+scp -i $rsaPath $homePath/project2_Delaunoy_Crasset_EXPLICIT.h $user@$server:$serverPath
 scp -i $rsaPath $homePath/project2_Delaunoy_Crasset_IO.c $user@$server:$serverPath
 scp -i $rsaPath $homePath/project2_Delaunoy_Crasset_IO.h $user@$server:$serverPath
 
@@ -22,8 +23,28 @@ scp -i $rsaPath $homePath/Scripts/test_multiple_computing_configurations.sh $use
 scp -i $rsaPath $homePath/Scripts/test_multiple_parameters_files.sh $user@$server:$serverPath/Scripts
 scp -i $rsaPath $homePath/Scripts/test_parameter_file.sh $user@$server:$serverPath/Scripts
 scp -i $rsaPath $homePath/Scripts/namesOfParameterFiles.txt $user@$server:$serverPath/Scripts
+scp -i $rsaPath $homePath/Scripts/test_weak_scaling.sh $user@$server:$serverPath/Scripts
+scp -i $rsaPath $homePath/Scripts/test_multiple_weak_scaling.sh $user@$server:$serverPath/Scripts
 
 # Parameters
 scp -i $rsaPath $homePath/Parameters/base_params_refraction.txt $user@$server:$serverPath/Parameters
 scp -i $rsaPath $homePath/Parameters/base_params_sriLanka.txt $user@$server:$serverPath/Parameters
+# scp -i $rsaPath $homePath/Parameters/1-params_sriLanka.txt $user@$server:$serverPath/Parameters
+# scp -i $rsaPath $homePath/Parameters/2-params_sriLanka.txt $user@$server:$serverPath/Parameters
+# scp -i $rsaPath $homePath/Parameters/4-params_sriLanka.txt $user@$server:$serverPath/Parameters
+# scp -i $rsaPath $homePath/Parameters/8-params_sriLanka.txt $user@$server:$serverPath/Parameters
+# scp -i $rsaPath $homePath/Parameters/16-params_sriLanka.txt $user@$server:$serverPath/Parameters
+
+scp -i $rsaPath $homePath/Parameters/mod_time_0,01.txt $user@$server:$serverPath/Parameters
+scp -i $rsaPath $homePath/Parameters/mod_space_2000.txt $user@$server:$serverPath/Parameters
+scp -i $rsaPath $homePath/Parameters/mod_time_1.txt $user@$server:$serverPath/Parameters
+scp -i $rsaPath $homePath/Parameters/mod_space_25000.txt $user@$server:$serverPath/Parameters
+scp -i $rsaPath $homePath/Parameters/mod_time_0,001.txt $user@$server:$serverPath/Parameters
+scp -i $rsaPath $homePath/Parameters/mod_time_0,1.txt $user@$server:$serverPath/Parameters
+scp -i $rsaPath $homePath/Parameters/mod_time_10.txt $user@$server:$serverPath/Parameters
+scp -i $rsaPath $homePath/Parameters/mod_space_1000.txt $user@$server:$serverPath/Parameters
+scp -i $rsaPath $homePath/Parameters/mod_space_10000.txt $user@$server:$serverPath/Parameters
+scp -i $rsaPath $homePath/Parameters/mod_time_0,05.txt $user@$server:$serverPath/Parameters
+scp -i $rsaPath $homePath/Parameters/mod_space_500.txt $user@$server:$serverPath/Parameters
+scp -i $rsaPath $homePath/Parameters/mod_space_250.txt $user@$server:$serverPath/Parameters
 
--- Scripts/test_remaining_configurations.sh
+++ Scripts/test_remaining_configurations.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-
 ############# VARIABLES TO MODIFY ############
 serverPath=/home/ulg/info0939/tcrasset/Project2
-nb_processes=4
-nb_threads=4
 ##############################################
+
 
 printf "Have you uploaded the needed files ? \n\n"
 printf ""
@@ -15,25 +13,28 @@ printf "For example, parameters in Parameters/ \n\n"
 printf "Did you change the files that you need to run ? \n\n"
 printf "Did you update 'namesOfParameterFiles' ? \n\n"
 
-
-
 read -r -p "Did you do all of the above ? [y/N]" response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
     
-    # Load modules and compile
     cd $serverPath
     module load openmpi
     mpicc -g $serverPath/project2_Delaunoy_Crasset*.c -std=c99 -lm -fopenmp -o $serverPath/waves
 
-    # Cycle through parameter filenames in namesOfParameterFiles.txt
-    # run a computation with it
-    while IFS="" read -r parameter_file || [ -n "$parameter_file" ]
-    do
-        echo "Job with parameter file $parameter_file, $nb_processes processes and $nb_threads threads"
-        sbatch --ntasks=$nb_processes --cpus-per-task=$nb_threads $serverPath/Scripts/test_parameter_file.sh $parameter_file
+    echo "Launching multiprocess_computation.sh"
 
-    done < $serverPath/Scripts/namesOfParameterFiles.txt
+    # for nb_threads in 1 2 4 8
+    # do
+    #     echo "Submitting job with 1 processes and $nb_threads threads"
+    #     sbatch --ntasks=1 --cpus-per-task=$nb_threads $serverPath/Scripts/test_computing_configuration.sh
+    # done
+
+    echo "Submitting job with 2 processes and 1 threads"
+    sbatch --ntasks=2 --cpus-per-task=1 $serverPath/Scripts/test_computing_configuration.sh
+    # echo "Submitting job with 2 processes and 2 threads"
+    # sbatch --ntasks=2 --cpus-per-task=2 $serverPath/Scripts/test_computing_configuration.sh
+    # echo "Submitting job with 4 processes and 1 threads"
+    # sbatch --ntasks=4 --cpus-per-task=1 $serverPath/Scripts/test_computing_configuration.sh
 else
     printf "\nExiting\n"
     exit 1
