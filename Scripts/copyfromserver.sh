5e44e8a247364601b4c18038ece6f648d1646b96
tcrasset <tomcrasset@gmail.com>
2019-12-19 21:16:09 +0100

    Modify several scripts

--- Scripts/copyfromserver.sh
+++ Scripts/copyfromserver.sh
@@ -9,4 +9,13 @@ serverPath=/home/ulg/info0939/tcrasset/Project2
 
 ssh-add $rsaPath
 # Copy whole Results/ folder from the Cluster to the Results folder on the personal PC
-scp -i $rsaPath -C -r $user@$server:$serverPath/Results/ $homePath/Results
+# scp -i $rsaPath -C -r $user@$server:$serverPath/Results/ $homePath/Results
+
+# scp -i $rsaPath $user@$server:$serverPath/Results/matrices_of_base_params_sriLanka_2_1/test.zip $homePath/Results
+
+
+# scp -i $rsaPath $user@$server:$serverPath/Results/statistics_for_configuration* $homePath/Results
+
+# scp -i $rsaPath $user@$server:$serverPath/Results/statistics_for_weakscaling* $homePath/Results
+
+scp -i $rsaPath $user@$server:$serverPath/Results/test.zip $homePath/Results
