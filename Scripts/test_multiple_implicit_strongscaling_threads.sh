095e7c9041cf6686cf42ea0b87a8fe8d03ed6249
tcrasset <tomcrasset@gmail.com>
2019-12-22 11:59:47 +0100

    Add Parameters and Scripts from master branch

---
+++ Scripts/test_multiple_implicit_strongscaling_threads.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+############# VARIABLES TO MODIFY ############
+serverPath=/home/ulg/info0939/tcrasset/Project2
+##############################################
+
+
+printf "Have you uploaded the needed files ? \n\n"
+printf ""
+printf "Have you put your uploaded files into the respective directories ? \n"
+printf ""
+printf "For example, parameters in Parameters/ \n\n"
+printf "Did you change the files that you need to run ? \n\n"
+printf "Did you update 'namesOfParameterFiles' ? \n\n"
+
+read -r -p "Did you do all of the above ? [y/N]" response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+    
+    cd $serverPath
+    module load openmpi
+    mpicc -g $serverPath/project2_Delaunoy_Crasset*.c -std=c99 -lm -fopenmp -o $serverPath/waves
+
+    echo "Launching multiprocess_computation.sh"
+
+    for nb_processes in 1
+    do
+        for nb_threads in 1 2 4 8 10 12 14 16
+        do
+            # let CPU_MEM="$MAX_MEM_PER_NODE/$nb_processes/$nb_threads"
+            # CPU_MEM="${CPU_MEM}G"
+            echo "Submitting job with $nb_processes processes and $nb_threads threads"
+            sbatch --ntasks=$nb_processes --cpus-per-task=$nb_threads $serverPath/Scripts/test_single_implicit_strongscaling.sh
+        done
+    done
+else
+    printf "\nExiting\n"
+    exit 1
+fi
---
+++ Scripts/test_multiple_weak_scaling.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+############# VARIABLES TO MODIFY ############
+serverPath=/home/ulg/info0939/tcrasset/Project2
+##############################################
+
+
+printf "Have you uploaded the needed files ? \n\n"
+printf ""
+printf "Have you put your uploaded files into the respective directories ? \n"
+printf ""
+printf "For example, parameters in Parameters/ \n\n"
+printf "Did you change the files that you need to run ? \n\n"
+printf "Did you update 'namesOfParameterFiles' ? \n\n"
+
+read -r -p "Did you do all of the above ? [y/N]" response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+    
+    cd $serverPath
+    module load openmpi
+    mpicc -g $serverPath/project2_Delaunoy_Crasset*.c -std=c99 -lm -fopenmp -o $serverPath/waves
+
+    echo "Launching multiprocess_computation.sh"
+
+    for nb_processes in 1
+    do
+        for nb_threads in 1 2 4 8 16
+        do
+            echo "Submitting job with $nb_processes processes and $nb_threads threads"
+            sbatch --ntasks=$nb_processes --cpus-per-task=$nb_threads $serverPath/Scripts/test_weak_scaling.sh
+        done
+    done
+else
+    printf "\nExiting\n"
+    exit 1
+fi
